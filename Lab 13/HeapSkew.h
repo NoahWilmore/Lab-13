eb8645b1fdc860678cfcdb250778036ffdb7346b
Jenny Haggerty <jmhaggerty42@students.tntech.edu>
2016-11-22 23:04:40 -0600

    seems to be working

--- Lab 13/HeapSkew.h
+++ Lab 13/HeapSkew.h
@@ -1,3 +1,13 @@
+/*
+	
+	Name: Jenny Haggerty && Noah Wilmore
+	
+	date: 11/22/16
+	
+	goal: Make a HeapSkew
+
+
+*/
 #if !defined HEAPSKEW_H
 #define HEAPSKEW_H
 
@@ -11,6 +21,9 @@ class HeapSkew : public Drawable
    private:
       BinaryTree<T>* bt;
       int sze;
+	  
+	  //pre: takes two subtrees to be merged
+	  //post: takes the two subtrees and recursivly puts the trees together
       BinaryTree<T>* merge(BinaryTree<T>* left, BinaryTree<T>* right);
 
       int (*compare_items) (T* item_1, T* item_2);
@@ -20,7 +33,13 @@ class HeapSkew : public Drawable
       ~HeapSkew();
 
       bool heapIsEmpty();
+	  
+	  //pre: takes the item to insert
+	  //post: inserts the item into the heap using the merge funtion
       void heapInsert(T* item);
+	  
+	  //pre: none
+	  //post: removes the beggest item in the tree from the heap using merge
       T* heapRemove();
 
       void draw(wxDC& dc, int width, int height);
@@ -54,32 +73,54 @@ template < class T >
 BinaryTree<T>* HeapSkew<T>::merge(BinaryTree<T>* left, BinaryTree<T>* right)
 {
   //DO THIS
-  BinaryTree<T>* leftRoot = left->getRootItem();
-  BinaryTree<T>* rightRoot = right->getRootItem();
-  BinaryTree<T>* LL = left->detachLeftSubtree();
-  BinaryTree<T>* LR = right->detachRightSubtree();
-  
-  if(left->isEmpty())
+  //checks to see if the subtress are empty. 
+  //if so deletes the empty returns the other
+  if(left->isEmpty()){
+	  
+	  delete left;
 	  return right;
-  else if(right->isEmpty())
+	  
+  } 
+  
+  if(right->isEmpty()){
+	  
+	  delete right;
 	  return left;
-  else if(leftRoot < rightRoot)
-	  merge(right, left);
+  
+  }
+  
+  //gets the items at the top and compraires them
+  T* leftRoot = left->getRootItem();
+  T* rightRoot = right->getRootItem();
+  
+  if ((*compare_items) (leftRoot, rightRoot) < 0){
+	  
+	  return merge(right, left);
+	  
+  }
+
+  //Makes trees to hold subtrees
+  BinaryTree<T>* LL = left->detachLeftSubtree();
+  BinaryTree<T>* LR = left->detachRightSubtree();
+  
+  left->attachRightSubtree(LL);
+  delete LL;
+
+  if(LR->isEmpty())
+  {
+	  delete LR;
+	  left->attachLeftSubtree(right);
+	  delete right;
+	  return left;
+  }
   else
   {
-	  left->attachRightSubtree(LL);
-	  if(LR->isEmpty())
-	  {
-		  left->attachLeftSubtree(right);
-		  return left;
-	  }
-	  else
-	  {
-		  BinaryTree<T>* leftSubtree = merge(LR, right);
-		  left->attachLeftSubtree(leftSubtree);
-		  return left;
-	  }
+	  BinaryTree<T>* leftSubtree = merge(LR, right);
+	  left->attachLeftSubtree(leftSubtree);
+	  delete leftSubtree;
+	  return left;
   }
+
 }
 
 template < class T >
@@ -87,12 +128,29 @@ void HeapSkew<T>::heapInsert(T* item)
 {
    //DO THIS (calls merge, should be short)
    
+	BinaryTree<T>* aTree = new BinaryTree<T>(item);
+	bt = merge(bt, aTree);
+	sze++;
+   
 }
 
 template < class T >
 T* HeapSkew<T>::heapRemove()
 {
    //DO THIS (calls merge, should be short)
+   
+	T* rootItem = bt->getRootItem();  
+
+	BinaryTree<T>* root = bt;
+	BinaryTree<T>* left = bt->detachLeftSubtree();
+	BinaryTree<T>* right = bt->detachRightSubtree();
+	
+	
+	bt = merge(left, right);
+	delete root;
+	sze--;
+
+	return rootItem;
    
 }
 
